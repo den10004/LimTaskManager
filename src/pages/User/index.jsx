cf373fed5ac9cf611835969e299c26bfc9bdaf34
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-09-08 16:40:48 +0300

    кнопка добавить ещё убирается, когда нечего отображать

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -12,11 +12,13 @@ function UserPage() {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [offset, setOffset] = useState(10);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
+  const [hasMore, setHasMore] = useState(true);
 
   const API_URL = import.meta.env.VITE_API_KEY;
 
-  const fetchTasks = async (newOffset) => {
+  const fetchTasks = async (newOffset, newLimit) => {
     try {
       const token = getCookie("authTokenPM");
       if (!token) {
@@ -24,7 +26,7 @@ function UserPage() {
       }
 
       const response = await fetch(
-        `${API_URL}/task?limit=20&offset=${newOffset}`,
+        `${API_URL}/task?limit=${newLimit}&offset=${newOffset}`,
         {
           method: "GET",
           headers: {
@@ -39,24 +41,32 @@ function UserPage() {
       }
 
       const data = await response.json();
-      setTasks((prevTasks) => [...prevTasks, ...data.items]);
+      setTasks((prevTasks) => {
+        const existingIds = new Set(prevTasks.map((task) => task.id));
+        const newTasks = data.items.filter((task) => !existingIds.has(task.id));
+        return [...prevTasks, ...newTasks];
+      });
+      setHasMore(data.items.length === newLimit);
       setLoading(false);
     } catch (err) {
       console.error(err.message);
       setError("Ошибка загрузки данных");
       setLoading(false);
+      setHasMore(false);
     }
   };
 
   useEffect(() => {
-    fetchTasks(offset);
+    fetchTasks(offset, limit);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleLoadMore = () => {
     const newOffset = offset + 10;
+    const newLimit = limit + 20;
     setOffset(newOffset);
-    fetchTasks(newOffset);
+    setLimit(newLimit);
+    fetchTasks(newOffset, newLimit);
   };
 
   return (
@@ -64,7 +74,7 @@ function UserPage() {
       <h3 className="h3-mtmb">Список задач</h3>
 
       {loading ? (
-        <div className="loading">Загрузка данных Ascension.pyданных...</div>
+        <div className="loading">Загрузка данных...</div>
       ) : error ? (
         <div className="error error-message">{error}</div>
       ) : (
@@ -93,9 +103,11 @@ function UserPage() {
           </tbody>
         </table>
       )}
-      <button className="create-btn" style={addBtn} onClick={handleLoadMore}>
-        Загрузить ещё
-      </button>
+      {hasMore && !loading && !error && (
+        <button className="create-btn" style={addBtn} onClick={handleLoadMore}>
+          Загрузить ещё
+        </button>
+      )}
     </section>
   );
 }
