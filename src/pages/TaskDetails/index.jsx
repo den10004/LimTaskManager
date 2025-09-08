ff28f2324c26729f5ed623c42762525b73c06a4c
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-09-08 12:01:27 +0300

    комментарии

--- src/pages/TaskDetails/index.jsx
+++ src/pages/TaskDetails/index.jsx
@@ -3,16 +3,40 @@ import { useState, useEffect } from "react";
 import { getCookie } from "../../utils/getCookies";
 import { formatDate } from "../../utils/dateUtils";
 
+const formStyle = {
+  marginTop: "10px",
+  width: "100%",
+};
+const formtext = {
+  width: "100%",
+};
+
+const commentsWrap = {
+  marginBottom: "15px",
+  padding: "10px",
+  backgroundColor: "#f5f5f5",
+  borderRadius: "5px",
+};
+
+const comments = {
+  fontSize: "12px",
+  color: "#666",
+  marginTop: "5px",
+};
+
 function TaskDetails() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [task, setTask] = useState(null);
+  const [comment, setComment] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [commentLoading, setCommentLoading] = useState(false);
 
   const fetchTaskById = async (id) => {
     const API_URL = import.meta.env.VITE_API_KEY;
     const token = getCookie("authTokenPM");
+
     if (!token) {
       throw new Error("Токен авторизации отсутствует");
     }
@@ -32,8 +56,51 @@ function TaskDetails() {
       const data = await response.json();
       return data;
     } catch (err) {
-      console.error("Error in fetchTaskById:", err);
+      console.error(err);
       throw new Error("Ошибка загрузки задачи", err);
+    }
+  };
+
+  const AddComments = async (e) => {
+    e.preventDefault();
+    setCommentLoading(true);
+    setError("");
+    const API_URL = import.meta.env.VITE_API_KEY;
+    const token = getCookie("authTokenPM");
+
+    if (!token) {
+      setError("Токен авторизации отсутствует");
+      setCommentLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(`${API_URL}/task/${id}/comments`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: comment }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Ошибка добавления комментария");
+      }
+
+      const newComment = await response.json();
+
+      setTask((prevTask) => ({
+        ...prevTask,
+        comments: [...prevTask.comments, newComment],
+      }));
+
+      setComment("");
+    } catch (err) {
+      console.error(err);
+      setError("Ошибка добавления комментария");
+    } finally {
+      setCommentLoading(false);
     }
   };
 
@@ -117,7 +184,7 @@ function TaskDetails() {
               </li>
               <li>
                 <b>Текст:</b> {task.title}
-              </li>{" "}
+              </li>
               {task.links && (
                 <li style={{ display: "flex" }}>
                   <b>Ссылки: </b>
@@ -136,8 +203,43 @@ function TaskDetails() {
                   </div>
                 </li>
               )}
+              <li style={{ display: "flex", flexDirection: "column" }}>
+                <b>Комментарии: </b>
+                <div style={{ marginLeft: "5px", marginTop: "10px" }}>
+                  {task.comments && task.comments.length > 0 ? (
+                    task.comments.map((comment, index) => (
+                      <div key={index} style={commentsWrap}>
+                        <div>{comment.text}</div>
+                        <div style={comments}>
+                          <b>Дата создания:</b> {formatDate(comment.created_at)}
+                        </div>
+                      </div>
+                    ))
+                  ) : (
+                    <div>Нет комментариев</div>
+                  )}
+                </div>
+              </li>
             </ul>
-          </div>{" "}
+
+            <form onSubmit={AddComments} style={formStyle}>
+              <textarea
+                style={formtext}
+                type="text"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                disabled={commentLoading}
+                required
+              />
+              <button
+                className="create-btn modal-button"
+                style={{ width: "200px" }}
+                disabled={commentLoading}
+              >
+                {commentLoading ? "Отправка..." : "Создать комментарий"}
+              </button>
+            </form>
+          </div>
         </>
       )}
     </div>
