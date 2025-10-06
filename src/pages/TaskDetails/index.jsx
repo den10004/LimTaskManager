c2af6ebe9a13ead4c1ef1ee6739e4eebc0cbb628
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-10-06 12:02:25 +0300

    исравлены ссылки

--- src/pages/TaskDetails/index.jsx
+++ src/pages/TaskDetails/index.jsx
@@ -9,8 +9,8 @@ import { useAuth } from "../../contexts/AuthContext";
 import Toast from "../../components/Toast";
 import AddFiles from "../../components/AddFiles";
 import CommentsSection from "../../components/Comments";
-import TaskInfoSection from "../../components/TaskInfoSection";
 import EditModal from "../../components/Modal/EditModal";
+import EditBtn from "../../components/UI/EditBtn";
 
 const styles = {
   container: {
@@ -25,10 +25,7 @@ const styles = {
     fontSize: "16px",
     marginBottom: "20px",
   },
-  taskTitle: {
-    margin: "0 0 20px 0",
-    color: "#333",
-  },
+
   taskContainer: {
     display: "grid",
     gridTemplateColumns: "1fr 1fr",
@@ -70,6 +67,42 @@ const styles = {
     borderRadius: "4px",
     margin: "20px 0",
   },
+  statusForm: {
+    display: "flex",
+    alignItems: "center",
+    flexWrap: "wrap",
+    gap: "10px",
+  },
+  flexAlignCenter: {
+    display: "flex",
+    alignItems: "center",
+    flexWrap: "wrap",
+    gap: "10px",
+  },
+  filesContainer: {
+    marginLeft: "5px",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "10px",
+  },
+  taskInfoList: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
+    listStyle: "none",
+    padding: 0,
+    margin: 0,
+  },
+  flexCenter: {
+    display: "flex",
+    alignItems: "center",
+  },
+  linksContainer: {
+    marginLeft: "5px",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "10px",
+  },
 };
 
 const URGENCY_COLORS = {
@@ -77,6 +110,161 @@ const URGENCY_COLORS = {
   medium: "orange",
   high: "var(--color-err)",
 };
+
+function TaskInfoSection({
+  task,
+  isAdmin,
+  getUserName,
+  getDirectionName,
+  formatDate,
+  normalizeLinks,
+  userPermissions,
+  getUrgencyColor,
+  loadings,
+  onUrgencyChange,
+  onDateChange,
+  onDescriptionChange,
+}) {
+  const MAX_URGENCY_STARS = 5;
+  console.log(task.links);
+
+  return (
+    <ul style={styles.taskInfoList}>
+      <li className="headlineBlock">
+        <b>Создатель:</b> {getUserName(task.created_by)}
+      </li>
+      <li className="headlineBlock">
+        <b>Ответственный:</b> {getUserName(task.assigned_user_id)}
+      </li>
+
+      <li className="headlineBlock">
+        <b>Срок выполнения:&nbsp;</b>
+        {formatDate(task.due_at) || "Не указано"}&nbsp;&nbsp;
+        {isAdmin && <EditBtn onDateChange={onDateChange} />}
+      </li>
+
+      <li className="headlineBlock">
+        <b>Направление:</b>{" "}
+        <div
+          style={{
+            display: "inline-block",
+            padding: "4px 8px",
+            backgroundColor: "#e7f1ff",
+            color: "var(--color-blue)",
+            borderRadius: "4px",
+          }}
+        >
+          {getDirectionName(task.direction_id)}
+        </div>
+      </li>
+      <li>
+        <div
+          className="headlineBlock"
+          style={{ borderBottom: "1px solid #e9ecef" }}
+        >
+          <b>Описание:</b>
+          {isAdmin && <EditBtn onDateChange={onDescriptionChange} />}
+        </div>
+        {task.description || "Не указано"}
+      </li>
+
+      <li className="headlineBlock" style={styles.flexCenter}>
+        <b>Важность:&nbsp;</b>
+        <div style={styles.flexCenter}>
+          {[...Array(MAX_URGENCY_STARS)].map((_, index) => {
+            const starValue = index + 1;
+            const isFilled = starValue <= task.urgency;
+
+            return (
+              <span
+                key={starValue}
+                onClick={() =>
+                  userPermissions.canEditUrgency &&
+                  !loadings.urgency &&
+                  onUrgencyChange(starValue)
+                }
+                style={{
+                  cursor: userPermissions.canEditUrgency
+                    ? loadings.urgency
+                      ? "not-allowed"
+                      : "pointer"
+                    : "not-allowed",
+                  color: isFilled ? getUrgencyColor(task.urgency) : "#ddd",
+                  fontSize: "20px",
+                  marginRight: "2px",
+                  opacity: userPermissions.canEditUrgency
+                    ? loadings.urgency
+                      ? 0.6
+                      : 1
+                    : 0.6,
+                  transition: "all 0.2s ease",
+                }}
+                title={`Установить важность: ${starValue}`}
+              >
+                {isFilled ? "★" : "☆"}
+              </span>
+            );
+          })}
+        </div>
+      </li>
+
+      {task.links && (
+        <li style={styles.flexCenter} className="headlineBlock">
+          <b>Ссылки:&nbsp;</b>
+          <div style={styles.linksContainer}>
+            {task.links.length === 0
+              ? "нет ссылок"
+              : task.links.map((link, index) => (
+                  <a
+                    key={index}
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ marginRight: "10px" }}
+                  >
+                    {" "}
+                    {link.url}
+                  </a>
+                ))}
+            {/*
+            {normalizeLinks(task.links).length === 0
+              ? "нет ссылок"
+              : normalizeLinks(task.links).map((link, index) => (
+                  <a
+                    key={index}
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ marginRight: "10px" }}
+                  >
+                    {link}
+                  </a>
+                ))}*/}
+          </div>
+        </li>
+      )}
+
+      {task.files && task.files.length > 0 && (
+        <li style={styles.flexCenter}>
+          <b>Файлы: </b>
+          <div style={styles.filesContainer}>
+            {task.files.map((file, index) => (
+              <div key={index} style={{ marginRight: "10px" }}>
+                <a
+                  href={`${import.meta.env.VITE_API_KEY}${file.file_url}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {file.file_name || "Файл без имени"}
+                </a>
+              </div>
+            ))}
+          </div>
+        </li>
+      )}
+    </ul>
+  );
+}
 
 const TaskDetails = () => {
   const { id: taskId } = useParams();
@@ -481,10 +669,46 @@ const TaskDetails = () => {
       <button style={styles.backButton} onClick={() => navigate("/task")}>
         ← Назад
       </button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+          alignItems: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <h3>
+          Задача #{task.id} - {task.title || "Не указано"}
+        </h3>
+        <div className="info">{task.status}</div>
 
-      <h3 style={styles.taskTitle}>
-        Задача #{task.id} - {task.title || "Не указано"}
-      </h3>
+        <form onSubmit={handleStatusUpdate} style={styles.statusForm}>
+          <select
+            id="status"
+            name="status"
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.target.value)}
+            required
+            disabled={loadings.status}
+          >
+            <option value="">Выберите статус</option>
+            {filteredStatuses.map((status, index) => (
+              <option key={index} value={status}>
+                {status}
+              </option>
+            ))}
+          </select>
+          <button
+            className="create-btn"
+            disabled={loadings.status || !selectedStatus}
+            type="submit"
+          >
+            {loadings.status ? "Обновление..." : "Обновить статус"}
+          </button>
+        </form>
+      </div>
+
       <div style={styles.taskContainer}>
         <div className="taskCard">
           <TaskInfoSection
@@ -500,11 +724,6 @@ const TaskDetails = () => {
             onUrgencyChange={handleUrgencyUpdate}
             onDateChange={() => setShowDatePicker(true)}
             onDescriptionChange={() => setDescriptionUpdate(true)}
-            selectedStatus={selectedStatus}
-            onStatusChange={setSelectedStatus}
-            statuses={filteredStatuses}
-            loadingStatus={loadings.status}
-            onStatusUpdate={handleStatusUpdate}
           />
 
           <form onSubmit={handleFileUpload} style={styles.form}>
