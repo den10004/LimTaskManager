889d197e652e734193875d1d82f3dfec7c404954
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-10-17 12:04:29 +0300

    исправлен стиль полей на iphone

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -232,13 +232,7 @@ function CreatePage() {
 
           <div className="create__block">
             <label htmlFor="due_at">Срок исполнения</label>
-            <div
-              style={{
-                maxWidth: "100%",
-                boxSizing: "border-box",
-                position: "relative",
-              }}
-            >
+            <div className="inputdate-wrapper">
               <input
                 type="datetime-local"
                 id="due_at"
@@ -249,6 +243,44 @@ function CreatePage() {
                 className="datetime-input"
                 required
               />
+              <span className="calendar-icon">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M16 2V6"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M8 2V6"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M3 10H21"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </span>
             </div>
           </div>
 
